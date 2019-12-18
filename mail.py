8dc866db576d22bb08a23cdde2725f46cb8131fe
LuckeyX7 <48850606+LuckeyX7@users.noreply.github.com>
2019-12-18 22:24:17 +0545

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+# Email you want to send the update from (notifications)
+fromEmail = 'email@gmail.com'
+fromEmailPassword = 'password'
+
+# Email you want to send the update to
+toEmail = 'email2@gmail.com'
+
+def sendEmail(image):
+	msgRoot = MIMEMultipart('related')
+	msgRoot['Subject'] = 'Security Update'
+	msgRoot['From'] = fromEmail
+	msgRoot['To'] = toEmail
+	msgRoot.preamble = 'Raspberry pi security camera update'
+
+	msgAlternative = MIMEMultipart('alternative')
+	msgRoot.attach(msgAlternative)
+	msgText = MIMEText('Smart Camera - Person Detected')
+	msgAlternative.attach(msgText)
+
+	msgText = MIMEText('<img src="cid:image1">', 'html')
+	msgAlternative.attach(msgText)
+
+	msgImage = MIMEImage(image)
+	msgImage.add_header('Content-ID', '<image1>')
+	msgRoot.attach(msgImage)
+
+	smtp = smtplib.SMTP('smtp.gmail.com', 587)
+	smtp.starttls()
+	smtp.login(fromEmail, fromEmailPassword)
+	smtp.sendmail(fromEmail, toEmail, msgRoot.as_string())
+	smtp.quit()
